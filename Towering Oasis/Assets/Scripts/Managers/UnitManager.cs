5b6035d47814bb7c91d16f0aaeb524f423f80103
Muhammad Ghalib <36465177+gabe108@users.noreply.github.com>
2018-10-05 13:00:18 +1000

    Added UnitManager.cs with working detection of two tiles for single unit
    Changes:
    - Now the grid can detect of two tiles for single unit
    
    To Do:
    - Complete Playercontroller
      Adding
      - Controller, PlayerController
    - Get player to work with pathfinding

--- Towering Oasis/Assets/Scripts/Managers/UnitManager.cs
+++ Towering Oasis/Assets/Scripts/Managers/UnitManager.cs
@@ -14,6 +14,14 @@ public class UnitManager : MonoBehaviour
             for (int j = 0; j < m_Parent.childCount; j++)
             {
                 m_Objects.Add(m_Parent.GetChild(j));
+                if(m_Parent.GetChild(j).childCount > 0 && m_Parent.name == "Obstacles")
+                {
+                    int objectCount = m_Objects.Count - 1;
+                    for (int x = 0; x < m_Objects[objectCount].childCount; x++)
+                    {
+                        m_Objects.Add(m_Parent.GetChild(j).GetChild(x));
+                    }
+                }
             }
         }
 
