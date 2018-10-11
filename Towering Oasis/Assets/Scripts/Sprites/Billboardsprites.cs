5d002f37e902a4510eeccfa0459d3a6e356d2f63
Muhammad Ghalib <36465177+gabe108@users.noreply.github.com>
2018-10-11 13:14:46 +1100

    Billboardsprite functionality added
    Changes:
    - BillBoard support added to game

---
+++ Towering Oasis/Assets/Scripts/Sprites/Billboardsprites.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboardsprites : MonoBehaviour
+{
+
+    public Transform MyCameraTransform;
+    private Transform MyTransform;
+    public bool alignNotLook = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        MyTransform = this.transform;
+        MyCameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (alignNotLook)
+            MyTransform.forward = MyCameraTransform.forward;
+        else
+            MyTransform.LookAt(MyCameraTransform, Vector3.up);
+    }
+}
