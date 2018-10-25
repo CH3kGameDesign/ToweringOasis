c0361087ab03985992de28e7e29cc3333d5f21ec
Nathan <nathan.a.p@hotmail.com>
2018-10-25 12:26:24 +1100

    Completed Healthbar

--- Towering Oasis/Assets/Scripts/HealthBar.cs
+++ Towering Oasis/Assets/Scripts/HealthBar.cs
@@ -5,33 +5,29 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     // Use this for initialization
-    public int m_MaxHP;
+    int m_MaxHP = 10;
     public int m_CurrentHP;
     public Transform m_HealthBar;
 
     void Start ()
     {
-		for(int i = 0; i < m_MaxHP; i++)
-        {
-            Transform go = Instantiate(m_HealthBar, new Vector3( -100, -100, 0), new Quaternion(0,0,0,0));
-            go.SetParent(transform.parent);
-            go.position = new Vector3(60 + 45 * i, 29) ;
-        }
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         var HP = GameObject.FindGameObjectsWithTag("HealthPoint");
-        foreach(var item in HP)
+        foreach (var item in HP)
         {
             Destroy(item);
         }
-            for (int i = 0; i < m_CurrentHP; i++)
-            {
-                Transform go = Instantiate(m_HealthBar, new Vector3(-100, -100, 0), new Quaternion(0, 0, 0, 0));
-                go.SetParent(transform.parent);
-                go.position = new Vector3(60 + 45 * i, 29);
-            }
-	}
+
+        for (int i = 0; i < m_CurrentHP && i < m_MaxHP; i++)
+        {
+            Transform go = Instantiate(m_HealthBar, new Vector3(-100, -100, 0), new Quaternion(0, 0, 0, 0));
+            go.SetParent(transform.parent);
+            go.position = new Vector3(100 + 45 * i, 25);
+        }
+    }
 }
