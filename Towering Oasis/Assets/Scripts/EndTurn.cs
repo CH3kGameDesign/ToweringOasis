bd3612d21ba8cfd74bdb7c335f5a1f244f72862e
Nathan <nathan.a.p@hotmail.com>
2018-11-15 15:54:12 +1100

    Added End Turn Button

---
+++ Towering Oasis/Assets/Scripts/EndTurn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTurn : MonoBehaviour {
+
+    // Use this for initialization
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void endTurn()
+    {
+        if (GameManager.Instance.m_bcontrolsAvailable == true)
+        {
+            //GameManager.Instance.m_bcontrolsAvailable = false;
+            //GameManager.Instance.m_nEnemiesAttacked = false;
+            //GameManager.Instance.m_nPlayerMoves = 0;
+            //GameManager.Instance.m_nEnemiesMoves = 0;
+            for (int x = 0; x < 4; x++)
+            {
+                UnitManager.Instance.m_Parent[0].GetChild(x).GetComponent<Actor>().m_bMoved = true;
+                UnitManager.Instance.m_Parent[0].GetChild(x).GetComponent<Actor>().m_bAttack = true;
+                UnitManager.Instance.m_Parent[0].GetChild(x).GetComponent<Actor>().m_bSkip = true;
+                GameManager.Instance.m_nPlayerMoves++;
+            }
+            GameManager.Instance.m_actions.SetActive(false);
+        }
+    }
+}
