9cf154751c01faa179d4096a612b0adcd2e154f1
Nathan <nathan.a.p@hotmail.com>
2018-11-22 11:09:48 +1100

    Fixed Can't Lose Bug

--- Towering Oasis/Assets/Scripts/Managers/States/GameState.cs
+++ Towering Oasis/Assets/Scripts/Managers/States/GameState.cs
@@ -16,23 +16,22 @@ public class GameState : BaseState
 	private void LateUpdate()
 	{
         if (SceneManager.GetActiveScene().buildIndex != 0)
-        { 
-            if (UnitManager.Instance.m_nPlayersAtExit == UnitManager.Instance.m_nPlayersAlive)
-            {
-                GameManager.Instance.m_LevelWonPanel.SetActive(true);
-                GameManager.Instance.ResetVariables();
-                GameManager.Instance.m_stateName = GameStates.LEVELWON;
-                GameManager.Instance.m_GameGUI.SetActive(false);
-            }
-
-            else if (UnitManager.Instance.m_nPlayersAlive <= 0)
+        {
+            if (UnitManager.Instance.m_nPlayersAlive <= 0)
             {
                 GameManager.Instance.m_GameOverPanel.SetActive(true);
                 GameManager.Instance.m_isGameOver = true;
                 GameManager.Instance.m_stateName = GameStates.GAMEOVER;
                 GameManager.Instance.m_GameGUI.SetActive(false);
-
                 GameManager.Instance.ResetVariables();
+            }
+
+            else if (UnitManager.Instance.m_nPlayersAtExit == UnitManager.Instance.m_nPlayersAlive)
+            {
+                GameManager.Instance.m_LevelWonPanel.SetActive(true);
+                GameManager.Instance.ResetVariables();
+                GameManager.Instance.m_stateName = GameStates.LEVELWON;
+                GameManager.Instance.m_GameGUI.SetActive(false);
             }
 
             else if (UnitManager.Instance.m_Parent[1].childCount <= 0 && GameManager.Instance.m_stateName == GameStates.GAME)
