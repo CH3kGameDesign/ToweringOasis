f9d83155f5ffdda6c5412d05ee0257e454df59c3
Muhammad Ghalib <36465177+gabe108@users.noreply.github.com>
2018-11-22 11:16:56 +1100

    Camera start level panning from exit to players

--- Towering Oasis/Assets/Scripts/CameraStartLevel.cs
+++ Towering Oasis/Assets/Scripts/CameraStartLevel.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class CameraStartLevel : MonoBehaviour
 {
+    public float m_lerpTime;
+    public float m_delay;
+    private bool m_lerpStart = false;
+
+    GameObject exit;
     Vector3 m_prevPos;
 
 	void Start ()
     {
-        m_prevPos = transform.position;        
-	}
+        m_prevPos = transform.position;
+
+        exit = GameObject.Find("M_TriggerExit (1)").transform.GetChild(0).transform.GetChild(0).gameObject;
+        transform.position = exit.transform.position;
+        Invoke("StartLerp", m_delay);
+    }
+
+    private void Update()
+    {
+        if (m_lerpStart)
+            transform.position = Vector3.Lerp(transform.position, m_prevPos, m_lerpTime);
+    }
+
+    private void StartLerp()
+    {
+        m_lerpStart = true;
+    }
 }
